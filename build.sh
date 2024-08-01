e62d292afe10fc21e6eb313004ca5f2b1bb59201
Thomas Solatges <thomas.solatges@orthopus.com>
2024-08-01 11:32:29 +0200

    (feat): WIP - pipeline

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ source /opt/ros/iron/setup.bash
 rosdep install -i -y --from-paths .
 source /opt/ros/iron/setup.bash
 rosdep install -i -y --from-paths .
-colcon build --symlink-install
+colcon build --symlink-install --continue-on-error
 source install/setup.bash
 #ros2 launch ros2_control_explorer explorer_spacenav.launch.py gui:=true
