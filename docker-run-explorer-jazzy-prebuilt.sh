377939be5031fe2b87c75d92bdad5d1d504d1f2e
Thomas Solatges <thomas.solatges@orthopus.com>
2025-05-28 13:56:02 +0200

    fix: do not share network with host to avoid persistant nodes on host

--- docker-run-explorer-jazzy-prebuilt.sh
+++ docker-run-explorer-jazzy-prebuilt.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 xhost +
-docker run --rm -it --name ros-jazzy-explorer --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v "/dev/:/dev" -v `pwd`:/src -ti ros-jazzy-explorer:prebuilt bash 
+docker run --rm -it --name ros-jazzy-explorer -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v "/dev/:/dev" -v `pwd`:/src -ti ros-jazzy-explorer:prebuilt bash 
