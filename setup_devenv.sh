c528400b066f23bc6715903152e9179b55644aee
Thomas Solatges <thomas.solatges@orthopus.com>
2025-06-16 16:45:25 +0200

    feat: cleanup / clone production branch by default

--- setup_devenv.sh
+++ setup_devenv.sh
@@ -5,5 +5,5 @@ mkdir -p src
 git clone git@github.com:ORTHOPUS-EXPLORER/explorer_docs.git
 mkdir src
 cd src
-git clone --recursive git@github.com:ORTHOPUS-EXPLORER/explorer_ws.git
+git clone --recursive -b prod/poc2 git@github.com:ORTHOPUS-EXPLORER/explorer_ws.git
 
