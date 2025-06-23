6dc727b4d2a7c6da7c7449ca9ea4f60cc4b00312
Thomas Solatges <thomas.solatges@orthopus.com>
2025-06-23 17:19:48 +0200

    fix: flash tools

--- tools/flash_restaure.sh
+++ tools/flash_restaure.sh
@@ -9,13 +9,13 @@ mkdir -p "$VESCPP"bench/log
 
 script -q -c "./proxyAll.sh bench fwinfo" /dev/stdout | tee "$VESCPP"bench/log/fwinfo_before.log
 sleep 5
-script -q -c "./proxyAll.sh bench o_comm stream_rate 0" /dev/stdout | tee "$VESCPP"bench/log/stream_rate.log
-sleep 5
 script -q -c "./saveAll_uuid.sh" /dev/stdout | tee "$VESCPP"bench/log/saveAll_uuid.log
 sleep 5
+script -q -c "./proxyAll.sh bench o_comm stream_rate 0" /dev/stdout | tee "$VESCPP"bench/log/stream_rate.log
+sleep 5
 script -q -c "./flash_all.sh" /dev/stdout | tee "$VESCPP"bench/log/flash_all.log
-echo "wait 30s, waiting  for VESCs to come back"
-sleep 30
+echo "wait 60s, waiting  for VESCs to come back"
+sleep 60
 script -q -c "./loadAll_uuid.sh" /dev/stdout | tee "$VESCPP"bench/log/loadAll_uuid.log
 sleep 1
 script -q -c "./loadAll_uuid.sh" /dev/stdout | tee "$VESCPP"bench/log/loadAll_uuid-2.log
