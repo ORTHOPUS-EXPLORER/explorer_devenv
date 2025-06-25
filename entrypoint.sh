ebd9bfd39daf8e91a232b4e0bb68ff3df538423a
Thomas Solatges <thomas.solatges@orthopus.com>
2025-06-25 14:27:02 +0200

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Set environment variables
 export ROS_LOCALHOST_ONLY=1
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+#export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 #export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
 #export CYCLONEDDS_URI=${CYCLONEDDS_URI:-'<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="lo"/></Interfaces><AllowMulticast>true</AllowMulticast></General><Discovery><ParticipantIndex>none</ParticipantIndex></Discovery></Domain></CycloneDDS><Gen><Allow>spdp</Allow></Gen>'}
 
@@ -10,7 +10,7 @@ export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 #ip link set lo multicast on
 
 # Adjust sysctl settings
-sysctl -w net.core.rmem_max=2147483647
+#sysctl -w net.core.rmem_max=2147483647
 
 # Execute the command passed to the container
 exec "$@"
